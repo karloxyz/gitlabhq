83fbef4671e5e886bcb8862f0c3830a06dae7a21
Karlo Soriano <dev+karlo@aelogica.com>
2013-04-25 08:43:19 +0800

    Total number of commits now gets graphed

--- app/controllers/stat_graph_controller.rb
+++ app/controllers/stat_graph_controller.rb
@@ -8,7 +8,7 @@ class StatGraphController < ProjectResourceController
   def show
   	@repo = @project.repository
     @stats = Gitlab::GitStats.new(@repo.raw, @repo.root_ref)
-    @display = @stats.total_commits
+    @display = @stats.total_commits.to_json
   end
   
 end
